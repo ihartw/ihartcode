90688ebfbc912efba245ef574a48607c7674db9e
ihartw <isaac.hartwick@gmail.com>
2024-11-02 19:09:44 -0400

    Added background glow for cursor

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,41 +1,55 @@
 <script setup>
-   import { onMounted, ref } from 'vue'
-   import 'materialize-css/dist/js/materialize.min'
-   import 'materialize-css/dist/css/materialize.min.css'
-   import ProjectCards from '../components/ProjectCards.vue'
-   import Hero from '../components/Hero.vue'
-   import About from '../components/About.vue'
-   import Skills from '../components/Skills.vue'
-   import Art from '../components/Art.vue'
-   import Experience from '../components/Experience.vue'
-   import Music from '../components/Music.vue'
+import { onMounted } from 'vue'
+import 'materialize-css/dist/js/materialize.min'
+import 'materialize-css/dist/css/materialize.min.css'
+import ProjectCards from '../components/ProjectCards.vue'
+import Hero from '../components/Hero.vue'
+import About from '../components/About.vue'
+import Skills from '../components/Skills.vue'
+import Art from '../components/Art.vue'
+import Experience from '../components/Experience.vue'
+import Music from '../components/Music.vue'
 
-   onMounted(() => {
-      M.AutoInit();
+onMounted(() => {
+   M.AutoInit();
+   let x = 0;
+   let y = 0;
+
+   function updateGradient() {
+      document.body.style.background = `radial-gradient(circle 600px at ${x}px ${y}px, #222126, #1a191d)`;
+      requestAnimationFrame(updateGradient);
+   }
+
+   document.addEventListener('mousemove', (event) => {
+      x = event.pageX;
+      y = event.pageY;
    });
+
+   updateGradient();
+});
 
 </script>
 
 <template>
-      <hero></hero>
-      <div class="container">
-         <about></about>
-         <skills></skills>
-         <experience></experience>
-         <project-cards></project-cards>
-         <art></art>
-         <music></music>
-      </div>
+   <hero></hero>
+   <div class="container">
+      <about></about>
+      <skills></skills>
+      <experience></experience>
+      <project-cards></project-cards>
+      <art></art>
+      <music></music>
+   </div>
 </template>
 
 <style>
-   .container {
-      width: 75% !important;
-   }
+.container {
+   width: 75% !important;
+}
 
-   @media screen and (max-width: 600px) {
-      .container {
-         width: 90% !important;
-      }
+@media screen and (max-width: 600px) {
+   .container {
+      width: 90% !important;
    }
+}
 </style>
