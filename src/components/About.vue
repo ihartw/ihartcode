4a697032e72e36dfcdffe4522d4d4140bafdb81f
ihartw <isaac.hartwick@gmail.com>
2024-11-02 15:40:05 -0400

    Added lottie image

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,24 +1,30 @@
 <script setup>
-   import { ref } from 'vue'
-   import { useIntersectionObserver, useAnimate } from '@vueuse/core'
+import { ref, onMounted } from 'vue'
+import { useIntersectionObserver, useAnimate } from '@vueuse/core'
+import { DotLottieVue } from '@lottiefiles/dotlottie-vue'
 
-   const isVisible = ref(false);
-   const about = ref(null);
-   const keyframes = [
-      { opacity: 0, transform: 'translateX(20px)'},
-      { opacity: 1 },
-   ]
-   
+const isVisible = ref(false);
+const about = ref(null);
+const playerRef = ref(null);
+const keyframes = [
+   { opacity: 0, transform: 'translateX(20px)' },
+   { opacity: 1 },
+]
+
+onMounted(() => {
+   const dotLottie = playerRef.value.getDotLottieInstance();
    useIntersectionObserver(about, ([{ isIntersecting }]) => {
-      if(!isVisible.value && isIntersecting) {
+      if (!isVisible.value && isIntersecting) {
          isVisible.value = true;
          useAnimate(about, keyframes, {
             duration: 1000,
             fill: 'forwards',
             easing: 'ease-in-out',
          });
+         dotLottie.play();
       }
    });
+})
 </script>
 
 <template>
@@ -27,14 +33,19 @@
          <div class="col m12 l6 push-l6">
             <h3 class="center-align">About Me</h3>
             <p>
-               I’m a passionate Software Engineer who thrives on problem-solving and crafting elegant web and mobile applications. Beyond coding, I enjoy playing musical instruments and drawing, which fuel my creativity. I love blending my artistic flair with my technical expertise to develop exceptional products and engaging user experiences. If you’re seeking a versatile engineer with a diverse skill set, I’d love to connect! Feel free to reach out on
+               I’m a passionate Software Engineer who thrives on problem-solving and crafting elegant web and mobile
+               applications. Beyond coding, I enjoy playing musical instruments and drawing, which fuel my creativity. I
+               love blending my artistic flair with my technical expertise to develop exceptional products and engaging
+               user experiences. If you’re seeking a versatile engineer with a diverse skill set, I’d love to connect! Feel
+               free to reach out on
             </p>
             <p>
                <a href="https://www.linkedin.com/in/isaac-hartwick-44b883117" target="_blank" class="link">LinkedIn</a>
             </p>
          </div>
          <div class="col m12 l6 pull-l6 web-apps">
-            <img src="../assets/images/web-apps.png" class="center-align">
+            <DotLottieVue style="height: 400px; width: 400px" ref="playerRef"
+               src="https://lottie.host/3dee8212-5a42-461e-ab33-9532ed53c712/vL3D8GDsrL.json" />
          </div>
       </div>
    </section>
@@ -46,8 +57,9 @@
    }
 
    .about .web-apps {
-      padding-top: 40px;
-      text-align: center;
+      display: flex;
+      justify-content: center;
+      align-items: center;
    }
 
    .about .web-apps img {
